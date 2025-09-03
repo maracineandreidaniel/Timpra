6ea53c657a441b5e4ea5f95d5c329bc7246c1ea6
maracineandreidaniel <maracineandreidaniel@gmail.com>
2025-09-03 19:36:36 +0300

    Removed usings

--- Timpra.BE/Timpra.DataAccess/Entities/Login.cs
+++ Timpra.BE/Timpra.DataAccess/Entities/Login.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Timpra.DataAccess.Entities
+﻿namespace Timpra.DataAccess.Entities
 {
     public class Login
     {
